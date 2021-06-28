c05a73dcbb44dfd83ce25ac8792f7462b35b8734
RoblesJ <robles.julioismael@gmail.com>
2021-06-28 03:17:05 -0300

    Overall rework trying to fix deployment issues

--- app.py
+++ app.py
@@ -1,35 +1,39 @@
-from flask import Flask 
-from flask_restful import Api 
+import os
+
+from flask import Flask
+from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
 from resources.user import UserRegister
-from resources.item import Item,ItemList
-from resources.store import Store,StoreList
-from db import db
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 
-
-# Define app settings with flask
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-# To allow flask propagating exception even if debug is set to false on app
+
+app.config['DEBUG'] = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'razhty'
 api = Api(app)
-# take the values from authenticate.py to verify identity through flas_JWT
-jwt = JWT(app, authenticate, identity)
 
+jwt = JWT(app, authenticate, identity)  # /auth
 
-
-
-#HTML redirections, and theyr resources.
+api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
-api.add_resource(UserRegister, '/register')
-api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
+api.add_resource(UserRegister, '/register')
+
 if __name__ == '__main__':
-    #TODO remove debug after production// def. port used 5000
+    from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=5000)
--- resources/user.py
+++ resources/user.py
@@ -2,24 +2,27 @@ import sqlite3
 from flask_restful import Resource, reqparse
 from models.user import UserModel
 
-
 class UserRegister(Resource):
-    # Take the data with flask's reqparse by the arguments we need to authenticate
+
     parser = reqparse.RequestParser()
-    parser.add_argument("username", type=str, required=True,
-                        help="This field can't be blank")
-    parser.add_argument("password", type=str, required=True,
-                        help="This field can't be blank")
+    parser.add_argument('username',
+        type=str,
+        required=True,
+        help="This field cannot be blank."
+    )
+    parser.add_argument('password',
+        type=str,
+        required=True,
+        help="This field cannot be blank."
+    )
 
     def post(self):
-        # Obtain data.
         data = UserRegister.parser.parse_args()
 
-        # Verify if the username already exists.
         if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists"}, 400
 
         user = UserModel(**data)
         user.save_to_db()
-        # Code 201, User created successfully
-        return {"message": "User created successfully"}, 201
+
+        return {"message": "User created successfully."}, 201
